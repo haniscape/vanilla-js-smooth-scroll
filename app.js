03ffb93ffcd97c2a0cb0c7cb2abb36e99537f45b
Hany Afifi <hanyafifi@gmail.com>
2020-08-28 19:07:04 +0200

    ADDED: first project

--- app.js
+++ app.js
@@ -1,6 +1,35 @@
 function smoothScroll(target, duration) {
 	var target = document.querySelector(target);
-	console.log(target);
+	var targetPosition = target.getBoundingClientRect().top;
+	var startPosition = windows.pageYOffset;
+	var distance = targetPosition - startPosition;
+	var startTime = null;
+
+	function animation(currentTime) {
+		if (startTime === null) startTime = currentTime;
+		var timeElapsed = currentTime - startTime;
+		var run = ease(timeElapsed, startPosition, distance, duration);
+		window.scrollTo(0, run);
+		if (timeElapsed < duration) requestAnimationFrame(animation);
+	}
+
+
+	requestAnimationFrame(animation);
 }
 
-smoothScroll('.beagle', 1000);
+
+
+function ease(t, b, c, d) {
+	t /= d;
+	return c * t * t + b;
+}
+
+
+
+var go = document.querySelector('.go');
+
+go.addEventListener('click', function () {
+	smoothScroll('.beagle', 1000);
+});
+
+
