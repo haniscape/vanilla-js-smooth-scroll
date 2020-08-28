4028937530ec8000fee8632fa1b405331311e16f
Hany Afifi <hanyafifi@gmail.com>
2020-08-28 23:09:44 +0200

    FIXED: js function and scroll timing

--- app.js
+++ app.js
@@ -1,35 +1,35 @@
+/* --------------------------------------------------
+smooth scroll down - using only Vanilla JavaScript
+------------------------------------------------- */
+
 function smoothScroll(target, duration) {
 	var target = document.querySelector(target);
 	var targetPosition = target.getBoundingClientRect().top;
-	var startPosition = windows.pageYOffset;
+	var startPosition = window.pageYOffset;
 	var distance = targetPosition - startPosition;
 	var startTime = null;
 
 	function animation(currentTime) {
 		if (startTime === null) startTime = currentTime;
 		var timeElapsed = currentTime - startTime;
-		var run = ease(timeElapsed, startPosition, distance, duration);
+		var run = easeDown(timeElapsed, startPosition, distance, duration);
 		window.scrollTo(0, run);
 		if (timeElapsed < duration) requestAnimationFrame(animation);
 	}
-
-
 	requestAnimationFrame(animation);
 }
 
 
+// Easing mathematics - credit: https://www.gizma.com/easing/
+function easeDown(t, b, c, d) {
+	t /= d / 2;
+	if (t < 1) return c / 2 * t * t * t * t + b;
+	t -= 2;
+	return -c / 2 * (t * t * t * t - 2) + b;
+};
 
-function ease(t, b, c, d) {
-	t /= d;
-	return c * t * t + b;
-}
 
-
-
-var go = document.querySelector('.go');
-
+var go = document.querySelector('#go');
 go.addEventListener('click', function () {
-	smoothScroll('.beagle', 1000);
+	smoothScroll('#siberian-husky', 3500);
 });
-
-
